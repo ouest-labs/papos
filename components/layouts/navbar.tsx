cd4c4493074e75bb06195fa1fd6ba914a0646de1
XYHub <lucien.loua@gmail.com>
2024-04-17 12:23:18 +0000

    fixed: metadata url

--- components/layouts/navbar.tsx
+++ components/layouts/navbar.tsx
@@ -6,7 +6,7 @@ import UserDropdown from "@/components/layouts/user-drop-down";
 const Navbar = async () => {
   const session = await auth();
   return (
-    <nav className="bg-background border-b text-foreground w-dvw p-5 fixed top-0">
+    <nav className="bg-background border-b text-foreground w-dvw p-5 sticky top-0">
       <div className="container mx-auto px-0 flex justify-between items-center">
         <div className="text-lg font-bold">Papos</div>
         <div className="space-x-3 flex items-center">
